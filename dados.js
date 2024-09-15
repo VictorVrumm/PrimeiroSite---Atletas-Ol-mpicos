19bb234c5be5c86b92e70f10f58185d54b747693
Victor Henrique <142319302+VictorVrumm@users.noreply.github.com>
2024-09-15 19:49:06 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,37 @@
+let dados = [
+  {
+    titulo: "Rayssa Leal",
+    descricao:
+      "Rayssa Leal, a Fada do Skate, é a mais jovem medalhista olímpica da história do Brasil. Com apenas 13 anos, ela conquistou a prata nos Jogos Olímpicos de Tóquio 2020, revolucionando o skate feminino no mundo. Sua habilidade e carisma a tornaram um ícone mundial do esporte, inspirando milhões de jovens a perseguirem seus sonhos.",
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags: "skatista Guarulhos sk8",
+  },
+  {
+    titulo: "Rebeca Andrade",
+    descricao:
+      "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros e o mundo esportivo com seu talento e determinação. Bicampeã olímpica, ela se tornou a maior medalhista da história do Brasil nos Jogos Olímpicos, acumulando diversas medalhas em diferentes categorias. Sua trajetória inspiradora, marcada por superação e dedicação, a transformou em um ícone do esporte nacional e um exemplo para as futuras gerações de atletas.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags: "ginastíca ouro Flamengo",
+  },
+  {
+    titulo: "Beatriz Souza",
+    descricao:
+      "Beatriz Souza é uma judoca brasileira que conquistou a primeira medalha de ouro do Brasil nos Jogos Olímpicos de Paris 2024. Sua vitória histórica na categoria acima de 78 kg a consagrou como um dos maiores nomes do judô brasileiro. Com sua força e determinação, Beatriz se tornou um exemplo para todos os atletas do país.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    tags: "luta pesada",
+  },
+  {
+    titulo: "Aline Silva",
+    descricao:
+      "Aline Silva é uma levantadora de peso brasileira, campeã mundial e olímpica. Conhecida por sua força e determinação, ela é um exemplo de superação e inspiração para muitas mulheres.",
+    link: "https://exemplo.com/aline-silva",
+    tags: "levantamento-de-peso força olímpica",
+  },
+  {
+    titulo: "Bruno Fratus",
+    descricao:
+      "Bruno Fratus é um nadador brasileiro, especialista em provas curtas. Ele conquistou diversas medalhas em competições internacionais e é um dos principais nomes da natação brasileira.",
+    link: "https://exemplo.com/bruno-fratus",
+    tags: "natação velocidade piscina",
+  },
+];
